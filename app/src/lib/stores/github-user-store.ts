578b30898fb9d12172a2ae053f8f605da4607cfb
x3325230 <70186980+x3325230@users.noreply.github.com>
2022-02-18 19:15:11 +0800

    Initialize releases/test-actions

---
+++ app/src/lib/stores/github-user-store.ts
@@ -0,0 +1,219 @@
+import { Account } from '../../models/account'
+import { GitHubRepository } from '../../models/github-repository'
+import { API } from '../api'
+import {
+  GitHubUserDatabase,
+  IMentionableUser,
+} from '../databases/github-user-database'
+
+import { compare } from '../compare'
+import { BaseStore } from './base-store'
+import { getStealthEmailForUser, getLegacyStealthEmailForUser } from '../email'
+
+/** Don't fetch mentionables more often than every 10 minutes */
+const MaxFetchFrequency = 10 * 60 * 1000
+
+/**
+ * The max time (in milliseconds) that we'll keep a mentionable query
+ * cache around before pruning it.
+ */
+const QueryCacheTimeout = 60 * 1000
+
+interface IQueryCache {
+  readonly repository: GitHubRepository
+  readonly users: ReadonlyArray<IMentionableUser>
+}
+
+/**
+ * The store for GitHub users. This is used to match commit authors to GitHub
+ * users and avatars.
+ */
+export class GitHubUserStore extends BaseStore {
+  private queryCache: IQueryCache | null = null
+  private pruneQueryCacheTimeoutId: number | null = null
+
+  public constructor(private readonly database: GitHubUserDatabase) {
+    super()
+  }
+
+  /**
+   * Retrieve a public user profile from the API based on the
+   * user login.
+   *
+   * @param account The account to use when querying the API
+   *                for information about the user
+   * @param login   The login (i.e. handle) of the user
+   */
+  public async getByLogin(
+    account: Account,
+    login: string
+  ): Promise<IMentionableUser | null> {
+    const api = API.fromAccount(account)
+    const apiUser = await api.fetchUser(login).catch(e => null)
+
+    if (!apiUser || apiUser.type !== 'User') {
+      return null
+    }
+
+    const email =
+      apiUser.email !== null && apiUser.email.length > 0
+        ? apiUser.email
+        : getStealthEmailForUser(apiUser.id, login, account.endpoint)
+
+    return {
+      avatarURL: apiUser.avatar_url,
+      email,
+      name: apiUser.name || apiUser.login,
+      login: apiUser.login,
+    }
+  }
+
+  /** Update the mentionable users for the repository. */
+  public async updateMentionables(
+    repository: GitHubRepository,
+    account: Account
+  ): Promise<void> {
+    assertPersisted(repository)
+
+    const api = API.fromAccount(account)
+
+    const cacheEntry = await this.database.getMentionableCacheEntry(
+      repository.dbID
+    )
+
+    if (
+      cacheEntry !== undefined &&
+      Date.now() - cacheEntry.lastUpdated < MaxFetchFrequency
+    ) {
+      return
+    }
+
+    const response = await api.fetchMentionables(
+      repository.owner.login,
+      repository.name,
+      cacheEntry?.eTag
+    )
+
+    if (response === null) {
+      await this.database.touchMentionableCacheEntry(
+        repository.dbID,
+        cacheEntry?.eTag
+      )
+      return
+    }
+
+    const { endpoint } = account
+
+    const mentionables = response.users.map(u => {
+      const { name, login, avatar_url: avatarURL } = u
+      const email = u.email || getLegacyStealthEmailForUser(login, endpoint)
+      return { name, login, email, avatarURL }
+    })
+
+    await this.database.updateMentionablesForRepository(
+      repository.dbID,
+      mentionables,
+      response.etag
+    )
+
+    if (
+      this.queryCache !== null &&
+      this.queryCache.repository.dbID === repository.dbID
+    ) {
+      this.queryCache = null
+      this.clearCachePruneTimeout()
+    }
+  }
+
+  /** Get the mentionable users in the repository. */
+  public async getMentionableUsers(
+    repository: GitHubRepository
+  ): Promise<ReadonlyArray<IMentionableUser>> {
+    assertPersisted(repository)
+    return this.database.getAllMentionablesForRepository(repository.dbID)
+  }
+
+  /**
+   * Get the mentionable users which match the text in some way.
+   *
+   * Hit results are ordered by how close in the search string
+   * they matched. Search strings start with username and are followed
+   * by real name. Only the first substring hit is considered
+   *
+   * @param text    A string to use when looking for a matching
+   *                user. A user is considered a hit if this text
+   *                matches any subtext of the username or real name
+   *
+   * @param maxHits The maximum number of hits to return.
+   */
+  public async getMentionableUsersMatching(
+    repository: GitHubRepository,
+    query: string,
+    maxHits: number = 100
+  ): Promise<ReadonlyArray<IMentionableUser>> {
+    assertPersisted(repository)
+
+    const users =
+      this.queryCache?.repository.dbID === repository.dbID
+        ? this.queryCache.users
+        : await this.getMentionableUsers(repository)
+
+    this.setQueryCache(repository, users)
+
+    const hits = []
+    const needle = query.toLowerCase()
+
+    // Simple substring comparison on login and real name
+    for (let i = 0; i < users.length && hits.length < maxHits; i++) {
+      const user = users[i]
+      const ix = `${user.login} ${user.name}`
+        .trim()
+        .toLowerCase()
+        .indexOf(needle)
+
+      if (ix >= 0) {
+        hits.push({ user, ix })
+      }
+    }
+
+    // Sort hits primarily based on how early in the text the match
+    // was found and then secondarily using alphabetic order. Ideally
+    // we'd use the GitHub user id in order to match dotcom behavior
+    // but sadly we don't have it handy here. The id property on IGitHubUser
+    // refers to our internal database id.
+    return hits
+      .sort(
+        (x, y) => compare(x.ix, y.ix) || compare(x.user.login, y.user.login)
+      )
+      .map(h => h.user)
+  }
+
+  private setQueryCache(
+    repository: GitHubRepository,
+    users: ReadonlyArray<IMentionableUser>
+  ) {
+    this.clearCachePruneTimeout()
+    this.queryCache = { repository, users }
+    this.pruneQueryCacheTimeoutId = window.setTimeout(() => {
+      this.pruneQueryCacheTimeoutId = null
+      this.queryCache = null
+    }, QueryCacheTimeout)
+  }
+
+  private clearCachePruneTimeout() {
+    if (this.pruneQueryCacheTimeoutId !== null) {
+      clearTimeout(this.pruneQueryCacheTimeoutId)
+      this.pruneQueryCacheTimeoutId = null
+    }
+  }
+}
+
+function assertPersisted(
+  repo: GitHubRepository
+): asserts repo is GitHubRepository & { dbID: number } {
+  if (repo.dbID === null) {
+    throw new Error(
+      `Need a GitHubRepository that's been inserted into the database`
+    )
+  }
+}
---
+++ app/src/ui/app-error.tsx
@@ -0,0 +1,296 @@
+import * as React from 'react'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DefaultDialogFooter,
+} from './dialog'
+import {
+  dialogTransitionEnterTimeout,
+  dialogTransitionLeaveTimeout,
+} from './app'
+import { GitError, isAuthFailureError } from '../lib/git/core'
+import { Popup, PopupType } from '../models/popup'
+import { CSSTransitionGroup } from 'react-transition-group'
+import { OkCancelButtonGroup } from './dialog/ok-cancel-button-group'
+import { ErrorWithMetadata } from '../lib/error-with-metadata'
+import { RetryActionType, RetryAction } from '../models/retry-actions'
+import { Ref } from './lib/ref'
+import memoizeOne from 'memoize-one'
+import { parseCarriageReturn } from '../lib/parse-carriage-return'
+
+interface IAppErrorProps {
+  /** The list of queued, app-wide, errors  */
+  readonly errors: ReadonlyArray<Error>
+
+  /**
+   * A callback which is used whenever a particular error
+   * has been shown to, and been dismissed by, the user.
+   */
+  readonly onClearError: (error: Error) => void
+  readonly onShowPopup: (popupType: Popup) => void | undefined
+  readonly onRetryAction: (retryAction: RetryAction) => void
+}
+
+interface IAppErrorState {
+  /** The currently displayed error or null if no error is shown */
+  readonly error: Error | null
+
+  /**
+   * Whether or not the dialog and its buttons are disabled.
+   * This is used when the dialog is transitioning out of view.
+   */
+  readonly disabled: boolean
+}
+
+/**
+ * A component which renders application-wide errors as dialogs. Only one error
+ * is shown per dialog and if multiple errors are queued up they will be shown
+ * in the order they were queued.
+ */
+export class AppError extends React.Component<IAppErrorProps, IAppErrorState> {
+  private dialogContent: HTMLDivElement | null = null
+  private formatGitErrorMessage = memoizeOne(parseCarriageReturn)
+
+  public constructor(props: IAppErrorProps) {
+    super(props)
+    this.state = {
+      error: props.errors[0] || null,
+      disabled: false,
+    }
+  }
+
+  public componentWillReceiveProps(nextProps: IAppErrorProps) {
+    const error = nextProps.errors[0] || null
+
+    // We keep the currently shown error until it has disappeared
+    // from the first spot in the application error queue.
+    if (error !== this.state.error) {
+      this.setState({ error, disabled: false })
+    }
+  }
+
+  private onDismissed = () => {
+    const currentError = this.state.error
+
+    if (currentError !== null) {
+      this.setState({ error: null, disabled: true })
+
+      // Give some time for the dialog to nicely transition
+      // out before we clear the error and, potentially, deal
+      // with the next error in the queue.
+      window.setTimeout(() => {
+        this.props.onClearError(currentError)
+      }, dialogTransitionLeaveTimeout)
+    }
+  }
+
+  private showPreferencesDialog = () => {
+    this.onDismissed()
+
+    //This is a hacky solution to resolve multiple dialog windows
+    //being open at the same time.
+    window.setTimeout(() => {
+      this.props.onShowPopup({ type: PopupType.Preferences })
+    }, dialogTransitionLeaveTimeout)
+  }
+
+  private onRetryAction = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    this.onDismissed()
+
+    const { error } = this.state
+
+    if (error !== null && isErrorWithMetaData(error)) {
+      const { retryAction } = error.metadata
+      if (retryAction !== undefined) {
+        this.props.onRetryAction(retryAction)
+      }
+    }
+  }
+
+  private renderErrorMessage(error: Error) {
+    const e = error instanceof ErrorWithMetadata ? error.underlyingError : error
+
+    if (e instanceof GitError) {
+      // See getResultMessage in core.ts
+      // If the error message is the same as stderr or stdout then we know
+      // it's output from git and we'll display it in fixed-width font
+      if (e.message === e.result.stderr || e.message === e.result.stdout) {
+        const formattedMessage = this.formatGitErrorMessage(e.message)
+        return <p className="monospace">{formattedMessage}</p>
+      }
+    }
+
+    return <p>{e.message}</p>
+  }
+
+  private getTitle(error: Error) {
+    if (isCloneError(error)) {
+      return 'Clone failed'
+    }
+
+    return 'Error'
+  }
+
+  private renderDialog() {
+    const error = this.state.error
+
+    if (!error) {
+      return null
+    }
+
+    return (
+      <Dialog
+        id="app-error"
+        type="error"
+        key="error"
+        title={this.getTitle(error)}
+        dismissable={false}
+        onSubmit={this.onDismissed}
+        onDismissed={this.onDismissed}
+        disabled={this.state.disabled}
+      >
+        <DialogContent onRef={this.onDialogContentRef}>
+          {this.renderErrorMessage(error)}
+          {this.renderContentAfterErrorMessage(error)}
+        </DialogContent>
+        {this.renderFooter(error)}
+      </Dialog>
+    )
+  }
+
+  private renderContentAfterErrorMessage(error: Error) {
+    if (!isErrorWithMetaData(error)) {
+      return undefined
+    }
+
+    const { retryAction } = error.metadata
+
+    if (retryAction && retryAction.type === RetryActionType.Clone) {
+      return (
+        <p>
+          Would you like to retry cloning <Ref>{retryAction.name}</Ref>?
+        </p>
+      )
+    }
+
+    return undefined
+  }
+
+  private onDialogContentRef = (ref: HTMLDivElement | null) => {
+    this.dialogContent = ref
+  }
+
+  private scrollToBottomOfGitErrorMessage() {
+    if (this.dialogContent === null || this.state.error === null) {
+      return
+    }
+
+    const e = getUnderlyingError(this.state.error)
+
+    if (isGitError(e)) {
+      if (e.message === e.result.stderr || e.message === e.result.stdout) {
+        this.dialogContent.scrollTop = this.dialogContent.scrollHeight
+      }
+    }
+  }
+
+  public componentDidMount() {
+    this.scrollToBottomOfGitErrorMessage()
+  }
+
+  public componentDidUpdate(
+    prevProps: IAppErrorProps,
+    prevState: IAppErrorState
+  ) {
+    if (prevState.error !== this.state.error) {
+      this.scrollToBottomOfGitErrorMessage()
+    }
+  }
+
+  private onCloseButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    this.onDismissed()
+  }
+
+  private renderFooter(error: Error) {
+    if (isCloneError(error)) {
+      return this.renderRetryCloneFooter()
+    }
+
+    const underlyingError = getUnderlyingError(error)
+
+    if (isGitError(underlyingError)) {
+      const { gitError } = underlyingError.result
+      if (gitError !== null && isAuthFailureError(gitError)) {
+        return this.renderOpenPreferencesFooter()
+      }
+    }
+
+    return this.renderDefaultFooter()
+  }
+
+  private renderRetryCloneFooter() {
+    return (
+      <DialogFooter>
+        <OkCancelButtonGroup
+          okButtonText={__DARWIN__ ? 'Retry Clone' : 'Retry clone'}
+          onOkButtonClick={this.onRetryAction}
+          onCancelButtonClick={this.onCloseButtonClick}
+        />
+      </DialogFooter>
+    )
+  }
+
+  private renderOpenPreferencesFooter() {
+    return (
+      <DialogFooter>
+        <OkCancelButtonGroup
+          okButtonText="Close"
+          onOkButtonClick={this.onCloseButtonClick}
+          cancelButtonText={__DARWIN__ ? 'Open Preferences' : 'Open options'}
+          onCancelButtonClick={this.showPreferencesDialog}
+        />
+      </DialogFooter>
+    )
+  }
+
+  private renderDefaultFooter() {
+    return <DefaultDialogFooter onButtonClick={this.onCloseButtonClick} />
+  }
+
+  public render() {
+    return (
+      <CSSTransitionGroup
+        transitionName="modal"
+        component="div"
+        transitionEnterTimeout={dialogTransitionEnterTimeout}
+        transitionLeaveTimeout={dialogTransitionLeaveTimeout}
+      >
+        {this.renderDialog()}
+      </CSSTransitionGroup>
+    )
+  }
+}
+
+function getUnderlyingError(error: Error): Error {
+  return isErrorWithMetaData(error) ? error.underlyingError : error
+}
+
+function isErrorWithMetaData(error: Error): error is ErrorWithMetadata {
+  return error instanceof ErrorWithMetadata
+}
+
+function isGitError(error: Error): error is GitError {
+  return error instanceof GitError
+}
+
+function isCloneError(error: Error) {
+  if (!isErrorWithMetaData(error)) {
+    return false
+  }
+  const { retryAction } = error.metadata
+  return retryAction !== undefined && retryAction.type === RetryActionType.Clone
+}
