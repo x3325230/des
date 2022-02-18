374b02a3073fd0ed91451663ab0efa1736c69159
x3325230 <70186980+x3325230@users.noreply.github.com>
2022-02-18 19:18:27 +0800

    Initialize releases/2.6.3-test6

---
+++ app/src/ui/changes/changes.tsx
@@ -0,0 +1,122 @@
+import * as React from 'react'
+import { ChangedFileDetails } from './changed-file-details'
+import {
+  DiffSelection,
+  IDiff,
+  ImageDiffType,
+  ITextDiff,
+} from '../../models/diff'
+import { WorkingDirectoryFileChange } from '../../models/status'
+import { Repository } from '../../models/repository'
+import { Dispatcher } from '../dispatcher'
+import { SeamlessDiffSwitcher } from '../diff/seamless-diff-switcher'
+import { PopupType } from '../../models/popup'
+
+interface IChangesProps {
+  readonly repository: Repository
+  readonly file: WorkingDirectoryFileChange
+  readonly diff: IDiff | null
+  readonly dispatcher: Dispatcher
+  readonly imageDiffType: ImageDiffType
+
+  /** Whether a commit is in progress */
+  readonly isCommitting: boolean
+  readonly hideWhitespaceInDiff: boolean
+
+  /**
+   * Called when the user requests to open a binary file in an the
+   * system-assigned application for said file type.
+   */
+  readonly onOpenBinaryFile: (fullPath: string) => void
+
+  /**
+   * Called when the user is viewing an image diff and requests
+   * to change the diff presentation mode.
+   */
+  readonly onChangeImageDiffType: (type: ImageDiffType) => void
+
+  /**
+   * Whether we should show a confirmation dialog when the user
+   * discards changes
+   */
+  readonly askForConfirmationOnDiscardChanges: boolean
+
+  /**
+   * Whether we should display side by side diffs.
+   */
+  readonly showSideBySideDiff: boolean
+
+  /** Called when the user opens the diff options popover */
+  readonly onDiffOptionsOpened: () => void
+}
+
+export class Changes extends React.Component<IChangesProps, {}> {
+  private onDiffLineIncludeChanged = (diffSelection: DiffSelection) => {
+    const file = this.props.file
+    this.props.dispatcher.changeFileLineSelection(
+      this.props.repository,
+      file,
+      diffSelection
+    )
+  }
+
+  private onDiscardChanges = (
+    diff: ITextDiff,
+    diffSelection: DiffSelection
+  ) => {
+    if (this.props.askForConfirmationOnDiscardChanges) {
+      this.props.dispatcher.showPopup({
+        type: PopupType.ConfirmDiscardSelection,
+        repository: this.props.repository,
+        file: this.props.file,
+        diff,
+        selection: diffSelection,
+      })
+    } else {
+      this.props.dispatcher.discardChangesFromSelection(
+        this.props.repository,
+        this.props.file.path,
+        diff,
+        diffSelection
+      )
+    }
+  }
+
+  public render() {
+    const diff = this.props.diff
+    const file = this.props.file
+    const isCommitting = this.props.isCommitting
+    return (
+      <div className="changed-file">
+        <ChangedFileDetails
+          path={file.path}
+          status={file.status}
+          diff={diff}
+          showSideBySideDiff={this.props.showSideBySideDiff}
+          onShowSideBySideDiffChanged={this.onShowSideBySideDiffChanged}
+          onDiffOptionsOpened={this.props.onDiffOptionsOpened}
+        />
+        <SeamlessDiffSwitcher
+          repository={this.props.repository}
+          imageDiffType={this.props.imageDiffType}
+          file={file}
+          readOnly={isCommitting}
+          onIncludeChanged={this.onDiffLineIncludeChanged}
+          onDiscardChanges={this.onDiscardChanges}
+          diff={diff}
+          hideWhitespaceInDiff={this.props.hideWhitespaceInDiff}
+          showSideBySideDiff={this.props.showSideBySideDiff}
+          askForConfirmationOnDiscardChanges={
+            this.props.askForConfirmationOnDiscardChanges
+          }
+          onOpenBinaryFile={this.props.onOpenBinaryFile}
+          onChangeImageDiffType={this.props.onChangeImageDiffType}
+        />
+      </div>
+    )
+  }
+
+  private onShowSideBySideDiffChanged = (showSideBySideDiff: boolean) => {
+    this.props.dispatcher.onShowSideBySideDiffChanged(showSideBySideDiff)
+  }
+}
