e0b9ffe1c9dd8b137e1842e536725e50de9bab9e
x3325230 <70186980+x3325230@users.noreply.github.com>
2022-02-18 19:27:34 +0800

    Initialize __release-test-d757fb5-401932162

---
+++ app/src/models/owner.ts
@@ -0,0 +1,20 @@
+/** The owner of a GitHubRepository. */
+export class Owner {
+  /**
+   * @param id The database ID. This may be null if the object wasn't retrieved from the database.
+   */
+  public constructor(
+    public readonly login: string,
+    public readonly endpoint: string,
+    public readonly id: number
+  ) {}
+
+  /**
+   * A hash of the properties of the object.
+   *
+   * Objects with the same hash are guaranteed to be structurally equal.
+   */
+  public get hash(): string {
+    return `${this.login}+${this.endpoint}+${this.id}`
+  }
+}
